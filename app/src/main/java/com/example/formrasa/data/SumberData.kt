c4a5a188e87455da47d648c46c2b38bb209f5a22
mhmdfakhrim <muhfakhri4@gmail.com>
2023-11-15 15:54:42 +0700

    latihan

---
+++ app/src/main/java/com/example/formrasa/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.formrasa.data
+
+import com.example.formrasa.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche,
+    )
+}
